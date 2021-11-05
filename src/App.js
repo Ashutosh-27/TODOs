96f7ae67e31029ea97aec283832f55871f26e5ee
Ashutosh <ashupotdar2001@gmail.com>
2021-11-05 13:33:02 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ function App() {
     var dd = String(date.getDate()).padStart(2, '0');
     var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = date.getFullYear();
-    date = mm + '/' + dd + '/' + yyyy;
+    date = yyyy + '/' + mm + '/' + dd;
   
 
     todos.map((e) => {
